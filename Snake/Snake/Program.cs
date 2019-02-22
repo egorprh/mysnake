54019518a2b495b825dab68162c862bde6bf0fa2
egorprh <yaga-2012@mail.ru>
2019-02-22 23:12:41 +0300

    Added function Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            /*int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';*/
+            Draw(4, 8, '#');
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
