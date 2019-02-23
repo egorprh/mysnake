1e30da7b19bb6dfe23a6f4e489df6942955f1763
egorprh <yaga-2012@mail.ru>
2019-02-23 15:51:02 +0300

    Create constructor Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,11 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 3;
-            p1.y = 5;
-            p1.sym = '*';
+            Point p1 = new Point(3, 5, '*');
             p1.Draw();
             
 
-            Point p2 = new Point();
-            p2.x = 6;
-            p2.y = 10;
-            p2.sym = '#';
+            Point p2 = new Point(6, 10, '#');
             p2.Draw();
 
             Console.ReadLine();
